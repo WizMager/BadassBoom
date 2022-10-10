182b62a0b2a9b1eb5a857596a66758ef688ee3cb
WizMager <wizmager@gmail.com>
2022-10-10 19:15:42 +0300

    feat: complete login and create/chosen room to connect

---
+++ Assets/Scripts/ComponentViews/RoomButtonComponents.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ComponentViews
+{
+    public class RoomButtonComponents : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text buttonText;
+
+        public TMP_Text GetButtonText => buttonText;
+    }
+}
